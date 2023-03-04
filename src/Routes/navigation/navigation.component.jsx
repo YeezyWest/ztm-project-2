73ea064cbd01a6ae1d54b7896deca2423b71033b
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2023-03-04 02:49:49 +0100

    update

---
+++ src/Routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from "react"
+import { Outlet, Link } from "react-router-dom"
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import './navigation.scss'
+const Navigation = ()=> {
+    return(
+      <Fragment>
+         <div className="navigation">
+         <Link className="logo-container" to='/'>
+           <CrwnLogo />
+         </Link>
+           <div className="nav-links-container">
+             <Link className="nav-link" to='/Shop'>
+               SHOP
+             </Link>
+           </div>
+         </div>
+         <Outlet />
+      </Fragment>
+    );
+  };
+  export default Navigation
