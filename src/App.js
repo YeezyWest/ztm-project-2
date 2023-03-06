9ae914a46db8bae642f32f3485777ea45cf639b1
Yusuf olatunji  adeshina <72082053+YeezyWest@users.noreply.github.com>
2023-03-06 02:02:04 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes,Route } from "react-router-dom";
 import Navigation from "./Routes/navigation/navigation.component";
 import Home from "./Routes/home/home.component";
+import SignIn from "./Routes/sign-in/sign-in.component";
 
 
 
@@ -16,6 +17,7 @@ const App = ()=> {
          <Route path="/" element={<Navigation />} >
          <Route index element={ <Home />} />
          <Route path="shop" element={<Shop/>} />
+         <Route path="sign-in" element={<SignIn/>} />
          </Route>
       </Routes>
      );
--- src/Routes/navigation/navigation.component.jsx
+++ src/Routes/navigation/navigation.component.jsx
@@ -14,6 +14,9 @@ const Navigation = ()=> {
              <Link className="nav-link" to='/Shop'>
                SHOP
              </Link>
+             <Link className="nav-link" to='/sign-in'>
+             SIGN-IN
+           </Link>
            </div>
          </div>
          <Outlet />
